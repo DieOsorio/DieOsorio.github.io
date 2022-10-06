5f3144a3d716ed05c88e4a246fa51ec05c97c5da
Diego Osorio <dieosorio@outlook.com>
2022-10-06 20:49:16 -0300

    Crash Course on Python added

--- src/data.js
+++ src/data.js
@@ -73,6 +73,15 @@ const data = {
       verifyAt: "https://coursera.org/verify/8GQ28F76ZP52",
     },
     {
+      name: "Crash Course on Python",
+      slug: "crash-course-on-python",
+      category: "Python",
+      image: "https://i.ibb.co/thRjfMh/Crash-Course-On-Python-2.jpg",
+      description:
+        "Thanks to a growing number of software programs, it seems as if anyone can make a webpage. But what if you actually want to understand how the page was created? There are great textbooks and online resources for learning web design, but most of those resources require some background knowledge. This course is designed to help the novice who wants to gain confidence and knowledge. We will explore the theory (what actually happens when you click on a link on a webpage?), the practical (what do I need to know to make my own page?), and the overlooked (I have a page, what do I do now?). Throughout the course there will be a strong emphasis on adhering to syntactic standards for validation and semantic standards to promote wide accessibility for users with disabilities.  The textbook we use is available online, “The Missing Link: An Introduction to Web Development and Programming” by Michael Mendez from www.opensuny.org. This course will appeal to a wide variety of people, but specifically those who would like a step-by-step description of the basics. There are no prerequisites for this course and it is assumed that students have no prior programming skills or IT experience. The course will culminate in a small final project that will require the completion of a very simple page with links and images. The focus of this course is on the basics, not appearance. You can see a sample final page at http://intro-webdesign.com/html5-plain.html. This is the first course in the Web Design For Everybody specialization. Subsequent courses focus on the marketable skills of styling the page with CSS3, adding interactivity with JavaScript and enhancing the styling with responsive design. You can see a sample site for the capstone course at http://intro-webdesign.com/",
+      verifyAt: "https://coursera.org/verify/G7C5Y6G6QW3G",
+    },
+    {
       name: "Java Programming: Arrays, Lists, And Structured Data",
       slug: "arrays-lists-and-structured-data",
       category: "Java",
